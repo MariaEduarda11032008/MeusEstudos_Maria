858f758329b5d4408abfa13d0fc834d1341a08db
Maria Eduarda <163295454+MariaEduarda11032008@users.noreply.github.com>
2024-11-08 11:27:14 -0300

    Add files via upload

---
+++ meus estudos_Maria/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é hmtl?',
+    'É uma linguagem de programação que define a estrutura do conteúdo da web. '
+)
+
+criaCartao(
+    'Biologia',
+    'O quqe é DNA?',
+    'É um ácido nucleico que apresenta todas as informações genéticas de um individuo'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Japonês',
+    'Como se "eu" em japonês ?',
+    '"eu" em japonês é Watahsi'
+)
